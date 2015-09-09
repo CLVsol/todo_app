b3d6810cf3458b83c8fdc7507ae2a9467327f3d7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-09 08:53:07 -0300

    Updated files "todo_app/todo_model.py" and "todo_app/todo_view.xml".

--- todo_app/todo_model.py
+++ todo_app/todo_model.py
@@ -8,3 +8,22 @@ class TodoTask(models.Model):
     name = fields.Char('Description', required=True)
     is_done = fields.Boolean('Done?')
     active = fields.Boolean('Active?', default=True)
+
+    @api.one
+    def do_toggle_done(self):
+        # print self.env.context
+        self.is_done = not self.is_done
+        return True
+
+    @api.multi
+    def do_clear_done(self):
+        done_recs = self.search([('is_done', '=', True)])
+        done_recs.write({'active': False})
+        return True
+
+    @api.one
+    def do_toggle_active(self):
+        # print self.env.context
+        self.active = not self.active
+        return True
+
