4a1e5fdb7601c4ae6a1a852850ccb11a24cdfdb0
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-10 08:02:40 -0300

    included file "todo_user/todo_task.py" and updated "todo_user/__init__.py".

---
+++ todo_user/todo_task.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _inherit = ['todo.task', 'mail.thread']
+
+    user_id = fields.Many2one('res.users', 'Responsible')
+    date_deadline = fields.Date('Deadline')
