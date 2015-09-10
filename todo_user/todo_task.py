753676f6ab483a77104cae0037479aa370e03124
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-10 08:16:02 -0300

    Updated "todo_user/todo_task.py".

--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -8,3 +8,19 @@ class TodoTask(models.Model):
 
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
+    name = fields.Char(help="What needs to be done?")
+
+    # @api.multi
+    # def do_clear_done(self):
+    #     done_recs = self.search([('is_done', '=', True)])
+    #     done_recs.write({'active': False})
+    #     return True
+
+    @api.multi
+    def do_clear_done(self):
+        domain = [('is_done', '=', True),
+                  '|', ('user_id', '=', self.env.uid),
+                       ('user_id', '=', False)]
+        done_recs = self.search(domain)
+        done_recs.write({'active': False})
+        return True
